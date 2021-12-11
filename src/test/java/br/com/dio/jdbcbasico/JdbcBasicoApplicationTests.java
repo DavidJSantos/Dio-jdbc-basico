e20e7be1ca223463dc11e282efadf0108c46f55f
DavidJSantos <davidjames1004@gmail.com>
2021-12-11 16:39:12 -0300

    Iniciando o projeto

---
+++ src/test/java/br/com/dio/jdbcbasico/JdbcBasicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.jdbcbasico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcBasicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
